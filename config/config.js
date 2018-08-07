d510853fb702d1a7bf47655ef6f4ce690dbf7428
opeyemi <opeyemionikute@yahoo.com>
2018-08-07 21:18:38 +0100

    refactor config to es6

--- config/config.js
+++ config/config.js
@@ -3,23 +3,24 @@ const dotenv = require('dotenv').config(); //require env variables to make file 
 const dbUser = process.env.DB_USER;
 const dbPassword = process.env.DB_PASSWORD;
 const dbBase = process.env.DB_BASE;
+const mongoURI = process.env.MONGODB_URI;
 
 module.exports = {
     db: {
         development: {
-            url: 'mongodb://' + dbUser + ':' + dbPassword + '@localhost/t-api',
+            url: `mongodb://${dbUser}:${dbPassword}@${dbBase}`,
             port: 5000
         },
         test: {
-            url: 'mongodb://' + dbUser + ':' + dbPassword + '@localhost/t-api-test',
+            url: `mongodb://${dbUser}:${dbPassword}@localhost/j-test`,
             port: 5000
         },
         staging: {
-            url: 'mongodb://' + dbUser + ':' + dbPassword + '@' + dbBase,
+            url: mongoURI || `mongodb://${dbUser}:${dbPassword}@${dbBase}`,
             port: 5300
         },
         production: {
-            url: 'mongodb://' + dbUser + ':' + dbPassword + '@' + dbBase,
+            url: mongoURI || `mongodb://${dbUser}:${dbPassword}@${dbBase}`,
             port: 5300
         }
     },
